3f202c7a0ace588ab7fa5f2f22f34b6c2b71b75e
Aufa Billah <aufaroot18@gmail.com>
2019-05-16 02:54:40 +0700

    Updates

---
+++ precache-manifest.a63d11b2654739b1e84f7d8fed872b45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651064f0d77e8e3de9ab6a0dfe3c7452",
+    "url": "/reactjs-todolist-app/index.html"
+  },
+  {
+    "revision": "08d230da509a40e85ad8",
+    "url": "/reactjs-todolist-app/static/css/main.ca092e4b.chunk.css"
+  },
+  {
+    "revision": "ff0ce0fce97f189f5456",
+    "url": "/reactjs-todolist-app/static/js/2.c32b159a.chunk.js"
+  },
+  {
+    "revision": "08d230da509a40e85ad8",
+    "url": "/reactjs-todolist-app/static/js/main.fd6b240e.chunk.js"
+  },
+  {
+    "revision": "86aa8fada7276d622743",
+    "url": "/reactjs-todolist-app/static/js/runtime~main.78d420b8.js"
+  }
+]);
